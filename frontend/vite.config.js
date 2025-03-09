6bcec8261979cb47975f35cef4da60b0ace40b02
samiaanastacio <bmiassa0108@gmail.com>
2025-03-09 18:41:13 -0300

    update Vite configuration to define base URL and enable build manifest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/', // Define a base URL para o projeto
   build: {
-    outDir: 'dist',
-  }
+    outDir: 'dist', // Define o diretório de saída para o build
+    manifest: true, // Gera um manifesto de build
+  },
+  plugins: [react()],
 })
